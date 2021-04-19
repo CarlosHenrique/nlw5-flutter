a2c540b8d9f4236f933f78f7dfffb7decf0362a7
CarlosHenrique <carlossouzap@outlook.com>
2021-04-19 20:32:40 -0300

    Aula 1, SplashPage e inicio da HomePage

---
+++ devquiz/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:DevQuiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+import 'package:DevQuiz/home/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
